850c99631df8de681bca99a1c0795635b12ab3cb
Dimitar Shuleski <dimitar.shuleski@outlook.com>
2025-11-03 20:59:32 +0100

    Add screenshots folder and update exam data

--- lib/data/exam_data.dart
+++ lib/data/exam_data.dart
@@ -1,14 +1,14 @@
 import '../models/exam.dart';
 
 final List<Exam> exams = [
-  Exam(subjectName: "Дискретна Математика", dateTime: DateTime(2025, 1, 12, 9, 0), rooms: ["Лаб 2"], passed: true),
-  Exam(subjectName: "Напредно Програмирање", dateTime: DateTime(2025, 1, 15, 10, 30), rooms: ["Лаб 12"], passed: true),
-  Exam(subjectName: "Структурно Програмирање", dateTime: DateTime(2025, 2, 5, 11, 0), rooms: ["Лаб 3"]),
-  Exam(subjectName: "Веб Дизајн", dateTime: DateTime(2025, 2, 10, 9, 30), rooms: ["Лаб 215"]),
-  Exam(subjectName: "Алгоритми и податочни структури", dateTime: DateTime(2025, 2, 12, 13, 0), rooms: ["Лаб 117"]),
-  Exam(subjectName: "Бази на податоци", dateTime: DateTime(2025, 2, 15, 10, 0), rooms: ["Лаб 215"]),
-  Exam(subjectName: "Оперативни системи", dateTime: DateTime(2025, 2, 20, 12, 0), rooms: ["Лаб 200В"]),
-  Exam(subjectName: "Компјутерски мрежи и безбедност", dateTime: DateTime(2025, 2, 25, 9, 0), rooms: ["Лаб 3"]),
-  Exam(subjectName: "Мобилни Информациски системи", dateTime: DateTime(2025, 3, 1, 11, 0), rooms: ["Лаб 215"]),
-  Exam(subjectName: "Веб програмирање", dateTime: DateTime(2025, 3, 5, 10, 0), rooms: ["Лаб 12"]),
+  Exam(subjectName: "Дискретна Математика", dateTime: DateTime(2025, 10, 29, 9, 0), rooms: ["Лаб 2"], passed: true),
+  Exam(subjectName: "Напредно Програмирање", dateTime: DateTime(2025, 10, 30, 10, 30), rooms: ["Лаб 12"], passed: true),
+  Exam(subjectName: "Структурно Програмирање", dateTime: DateTime(2025, 11, 20, 11, 0), rooms: ["Лаб 3"]),
+  Exam(subjectName: "Веб Дизајн", dateTime: DateTime(2025, 11, 21, 9, 30), rooms: ["Лаб 215"]),
+  Exam(subjectName: "Алгоритми и податочни структури", dateTime: DateTime(2025, 11, 21, 13, 0), rooms: ["Лаб 117"]),
+  Exam(subjectName: "Бази на податоци", dateTime: DateTime(2025, 11, 24, 10, 0), rooms: ["Лаб 215"]),
+  Exam(subjectName: "Оперативни системи", dateTime: DateTime(2025, 11, 25, 12, 0), rooms: ["Лаб 200В"]),
+  Exam(subjectName: "Компјутерски мрежи и безбедност", dateTime: DateTime(2025, 11, 28, 9, 0), rooms: ["Лаб 3"]),
+  Exam(subjectName: "Мобилни Информациски системи", dateTime: DateTime(2025, 11, 29, 11, 0), rooms: ["Лаб 215"]),
+  Exam(subjectName: "Веб програмирање", dateTime: DateTime(2025, 11, 30, 10, 0), rooms: ["Лаб 12"]),
 ];
